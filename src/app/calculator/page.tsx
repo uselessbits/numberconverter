f60c9c02e399a72b038e0f94aeeec4e0d38ce4f3
root <root@DESKTOP-72G8BL1>
2024-02-18 10:49:13 +0200

    Stable version

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,41 +1,179 @@
-'use client'
+"use client"
 import React from 'react'
 import { useState } from 'react'
+
 
 export default function Calculator() {
 
 
-    const [first, setFirst] = useState('')
-    const [second, setSecond] = useState('')
-    const [base, setBase] = useState('')
-    const [operator, setOperator] = useState('')
+    let [first, setFirst] = useState('')
+    let [second, setSecond] = useState('')
+    let [base, setBase] = useState(0)
+    let [operator, setOperator] = useState('+')
+    let [result, setResult] = useState('')
 
 
     function add () {
 
-    }
-    function sub () {
+    
+    let len_first = first.length; 
+    let a, b, len_a, len_b;
+    let len_second = second.length; 
+    let sum, s; 
+    s = ""; 
+    sum = ""; 
+    let diff; 
+    a=first;
+    b=second;
+    len_a = a.length; 
+    len_b = b.length; 
+   
 
+    diff = Math.abs(len_first - len_second); 
+
+    for(let i = 1; i <= diff; i++) 
+        s += "0"; 
+       
+    // Condition to check if the strings 
+    // have lengths mis-match 
+    if (len_a < len_b) 
+        a = s + a; 
+    else
+        b = s + b; 
+   
+    let curr, carry = 0; 
+       
+    // Loop to find the find the sum 
+    // of two integers of base B 
+    for(let i = Math.max(len_a, len_b) - 1; 
+            i > -1; i--) 
+    { 
+         
+        // Current Place value for 
+        // the resultant sum 
+        curr = carry + (a.charCodeAt(i) - '0'.charCodeAt(0)) + (b.charCodeAt(i) - '0'.charCodeAt(0)); 
+   
+        // Update carry 
+        carry = parseInt(curr / base, 10);
+   
+        // Find current digit 
+        curr = curr % base; 
+   
+        // Update sum result 
+        sum = String.fromCharCode(
+            curr + '0'.charCodeAt(0)) + sum; 
+    } 
+    if (carry > 0) 
+        sum = String.fromCharCode(
+            carry + '0'.charCodeAt(0)) + sum; 
+             
+    setResult(sum)
+    } 
+
+    function sub() {
+
+
+        
+        let len_first = first.length;
+        let len_second = second.length;
+        let diff = Math.abs(len_first - len_second);
+        let s = "";
+        let r = "";
+        let a = first;
+        let b = second;
+        let borrow = 0;
+  
+
+        for(let i = 1; i <= diff; i++) {
+            s += "0";
+        }
+    
+        if (len_first < len_second) {
+            a = s + a;
+        } else {
+            b = s + b;
+        }
+    
+        for(let i = Math.max(len_first, len_second) - 1; i > -1; i--) {
+            let curr = (a.charCodeAt(i) - '0'.charCodeAt(0)) - (b.charCodeAt(i) - '0'.charCodeAt(0)) + borrow;
+    
+            if (curr < 0) {
+                curr += base;
+                borrow = -1;
+            } else {
+                borrow = 0;
+            }
+    
+            r = String.fromCharCode(curr + '0'.charCodeAt(0)) + r;
+        }
+    
+        if (borrow < 0) {
+            r = String.fromCharCode(Math.abs(borrow) + '0'.charCodeAt(0)) + r;
+        }
+    
+        setResult(r);
     }
+
+
+    
     function mul () {
 
+
+        let carry = 0;
+        let r = "";
+
+        for(let i = first.length - 1; i >= 0; i--) {
+            let temp = parseInt(first[i], 10) * parseInt(second, 10) + carry;
+            r = (temp % base).toString(base)+r;
+            carry = Math.floor(temp / base);
+          
+        }
+        if (carry != 0) {
+            r = carry.toString(base) + r;
+        }
+        setResult(r);
     }
     function div () {
-
+        let r = "";
+        let temp = 0;
+        let digit = parseInt(second, base);
+    
+        for (let i = 0; i < first.length; i++) {
+            temp = temp * base + (first.charCodeAt(i) - '0'.charCodeAt(0));
+    
+            if (temp < digit) {
+                r += "0";
+                continue;
+            }
+    
+            let quotient = Math.floor(temp / digit);
+            r += quotient.toString();
+            temp = temp % digit;
+        }
+    
+        // Remove leading zeros
+        r = r.replace(/^0+/, '');
+    
+        setResult(r);
     }
 
     function handleCalculation() {
-        let result
+        
         try {
-            
-            if (operator == '0')
-                result = add()
-            if (operator == '1')
-                result = sub()
-            if (operator == '2')
-                result = mul()
-            if (operator == '3')
-                result = div()
+            if (first>=base || second>=base) {
+                setResult('Invalid input')
+                return
+            }
+        
+            console.log(first, second, base, operator)
+            if (operator == '+')
+                add()
+            if (operator == '-')
+                sub()
+            if (operator == '*')
+                mul()
+            if (operator == '/')
+                div()
 
         } catch (error) {
             
@@ -50,19 +188,20 @@ export default function Calculator() {
         <div>
             <h1>Calculator</h1>
             <h1>First Number</h1>
-            <input value={first} onChange={(e) => setFirst(e.target.value)}></input>
+            <input type='number' min='0' value={first} onChange={(e) => setFirst(e.target.value)}></input>
             <h1>Second Number</h1>
-            <input value={second} onChange={(e) => setSecond(e.target.value)}></input>
+            <input type='number' min='0' value={second} onChange={(e) => setSecond(e.target.value)}></input>
             <h1 >Base</h1>
-            <input value={base} onChange={(e) => setBase(e.target.value)}></input>
+            <input type='number' min='0' value={base} onChange={(e) => setBase(e.target.value)}></input>
 
             <select value={operator} onChange={(e)=> setOperator(e.target.value)}>
-                <option value='0'>+</option>
-                <option value='1'>-</option>
-                <option value='2'>*</option>
-                <option value='3'>/</option>
+                <option value='+'>+</option>
+                <option value='-'>-</option>
+                <option value='*'>*</option>
+                <option value='/'>/</option>
             </select>
-            <button onClick={handleCalculation}>Submit</button>
+            <button className='button' onClick={handleCalculation}>Submit</button>
+            <text>Result:{result}</text>
 
         </div>
     </div>
--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,29 +1,194 @@
-import React from 'react'
+"use client"
+
+import { useState } from 'react'
+import { checkIfNumberValid } from '../utils'
+
+
+
 
 export default function page() {
+
+
+  
+  let [number, setNumber] = useState('')
+  let [source, setSource] = useState('')
+  let [destination, setDestination] = useState('')
+  let [result, setResult] = useState('')  
+
+
+  function division(num){
+    
+    let r = "";
+    let d = parseInt(destination, 10)
+
+    do{
+
+    
+    r = (num % d).toString(d) + r
+    num = Math.floor(num / d)
+
+    }while(num > 0)
+
+
+    return r
+        
+  }
+
+
+  function substitution(num){
+    let r = 0;
+    let s = parseInt(source, 10)
+    let power = 0;
+
+    for(let i = num.length - 1; i >= 0; i--){
+      r += (parseInt(num[i], 10)) * (s ** power)
+      power += 1
+    }
+  
+  
+    return r.toString()
+  }
+
+
+
+  function binaryToHex(binary) {
+    // Pad the binary number with zeros on the left to make its length a multiple of 4
+    while (binary.length % 4 !== 0) {
+        binary = '0' + binary;
+    }
+
+    // Split the binary number into groups of 4 digits
+    let groups = binary.match(/.{1,4}/g);
+
+    // Convert each group to a hexadecimal digit
+    let hex = groups.map(group => parseInt(group, 2).toString(16)).join('');
+
+    return hex;
+}
+
+function binaryToOctal(binary) {
+  // Pad the binary number with zeros on the left to make its length a multiple of 3
+  while (binary.length % 3 !== 0) {
+      binary = '0' + binary;
+  }
+
+  // Split the binary number into groups of 3 digits
+  let groups = binary.match(/.{1,3}/g);
+
+  // Convert each group to an octal digit
+  let octal = groups.map(group => parseInt(group, 2).toString(8)).join('');
+
+  return octal;
+}
+
+function binaryToBase4(binary) {
+  // Pad the binary number with zeros on the left to make its length a multiple of 2
+  while (binary.length % 2 !== 0) {
+      binary = '0' + binary;
+  }
+
+  // Split the binary number into groups of 2 digits
+  let groups = binary.match(/.{1,2}/g);
+
+  // Convert each group to a base 4 digit
+  let base4 = groups.map(group => parseInt(group, 2).toString(4)).join('');
+
+  return base4;
+}
+
+function hexToBinary(hex) {
+  console.log(hex)
+  let binary = hex.split('').map(digit => parseInt(digit, 16).toString(2).padStart(4, '0')).join('');
+  return binary;
+}
+function octalToBinary(octal) {
+  let binary = octal.split('').map(digit => parseInt(digit, 8).toString(2).padStart(3, '0')).join('');
+  return binary;
+}
+function base4ToBinary(base4) {
+  let binary = base4.split('').map(digit => parseInt(digit, 4).toString(2).padStart(2, '0')).join('');
+  return binary;
+}
+
+  function handleConversion(){
+
+    if(checkIfNumberValid(number, parseInt(source, 10)) === false){
+    setResult('Invalid number in given base')
+    return
+    }
+
+    if(source === destination){
+      setResult(number)
+    }
+    else if(source === '10'){
+      setResult(division(parseInt(number, 10)) )
+    }
+    else if(destination === '10'){
+      setResult(substitution(number))
+    }
+    else{
+
+      if(source === '2'){
+      
+        if (destination === '4'){
+          setResult(binaryToBase4(number))
+
+        }
+        else if (destination === '8'){
+          setResult(binaryToOctal(number))
+        }
+        else if (destination === '16'){
+          setResult(binaryToHex(number))
+        }
+
+      
+      }
+      else if(destination === '2'){
+        if (source === '4'){
+
+          setResult(base4ToBinary(number))
+        }
+        else if (source === '8'){
+          setResult(octalToBinary(number))
+        }
+        else if (source === '16'){
+          setResult(hexToBinary(number))
+        }
+      }
+      else
+      {
+      let temp = substitution(number)
+      setResult(division(parseInt(temp, 10)))
+      }
+    }
+
+
+  }
+
+
+
+
+
   return (
     
     <div>
       
       <h1>Converter</h1>
       
-      <form className='flex'>
+      
       <h1>Enter a number</h1>
-      <input></input>
+      <input min='0' value={number} onChange={(e) => setNumber(e.target.value)}></input>
       <h1>From base</h1>
-      <select name='first'>
-      <option value="0">2</option>
-      <option value="0">3</option>
-      <option value="0">4</option>
-      <option value="0">5 </option>
-      </select>
+      <input type='number' min='0' value={source} onChange={(e) => setSource(e.target.value)}></input>
       
       <h1>To base</h1>
-      <select ></select>
-      
+      <input type='number' min='0' value={destination} onChange={(e) => setDestination(e.target.value)}></input>
+      <text>Result:{result}</text>
+      <button className='button' onClick={handleConversion}>Submit</button>
 
-       </form>
+       
       </div>
+
     
     
   )
