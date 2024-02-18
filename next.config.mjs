3b0919846b148a4f45fa84666879fd3d42c5a340
uselessbits <velapaul.vp@gmail.com>
2024-02-18 13:42:18 +0200

    FInal

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    output:'export',
   };
-  
-  module.exports = nextConfig;
+  /* module.exports = nextConfig*/
+  export default nextConfig;
   
